5f1b864f238b45a062445342ebe0611769160d1d
kawauchi toma <juju4415@icloud.com>
2023-05-21 23:06:19 +0900

    環境を実装

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/toMaTo0502/monkey-interpreter-sample/evaluator"
 	"github.com/toMaTo0502/monkey-interpreter-sample/lexer"
+	"github.com/toMaTo0502/monkey-interpreter-sample/object"
 	"github.com/toMaTo0502/monkey-interpreter-sample/parser"
 )
 
@@ -27,6 +28,7 @@ const MONKEY_FACE = `            __,__
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Print(PROMPT)
@@ -45,7 +47,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
